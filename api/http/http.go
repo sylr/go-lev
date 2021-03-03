80f57194f826c6bc5e47d419cbdc6796858ebbb7
Sylvain Rabot <sylvain@abstraction.fr>
2021-03-03 10:08:41 +0100

    Turn off readiness upon SIGTERM
    Signed-off-by: Sylvain Rabot <sylvain@abstraction.fr>

--- api/http/http.go
+++ api/http/http.go
@@ -3,7 +3,10 @@ package http
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 
 	v1 "github.com/sylr/go-lev/api/http/v1"
 
@@ -25,6 +28,20 @@ func InitializeEndpoints() {
 	http.HandleFunc("/stop", httpGetStop)
 	http.HandleFunc("/ready", httpGetReady)
 	http.HandleFunc("/ping", httpGetPing)
+
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		select {
+		case <-signals:
+			mutex.Lock()
+			defer mutex.Unlock()
+			fmt.Println("SIGTERM received, stop sending 200 OK on /ready.")
+
+			Stopped = true
+			signal.Stop(signals)
+		}
+	}()
 }
 
 func httpGetStart(w http.ResponseWriter, r *http.Request) {
